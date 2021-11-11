b9701062c4427220815cf62c271530cfd55c1dde
Daan Meijer <daansander02@outlook.com>
2021-11-11 15:54:51 +0100

    Forgot to commit

--- src/main.c
+++ src/main.c
@@ -1,21 +1,6 @@
-#include <stdio.h>
 #include "../include/ft_printf.h"
-
-//void test(const char *format, t_format_info *formatInfo, ...)
-//{
-//	va_list	args;
-//
-//	va_start(args, formatInfo);
-//	get_format_info(format, formatInfo, args);
-//	va_end(args);
-//}
 
 int main(void)
 {
-//	t_format_info formatInfo;
-//
-//	test("-0#+ *.*c", &formatInfo, 999, 42);
-////	get_format_info("-0#+ *.*c", &formatInfo);
-//	print_format_info(&formatInfo);
 	ft_printf("Hallo, dit is %% een %c\n", 'c');
 }
