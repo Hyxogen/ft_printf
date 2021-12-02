23d78a3d812275ba61f26b52df8cc6a0407ea803
Daan Meijer <daansander02@outlook.com>
2021-12-02 10:30:50 +0100

    Cleanup format_info_utils.h

--- src/format_info_utils.h
+++ src/format_info_utils.h
@@ -1,9 +1,8 @@
 #ifndef FORMAT_INFO_UTILS_H
-#define FORMAT_INFO_UTILS_H
+# define FORMAT_INFO_UTILS_H
 
-#include <stdint.h>
-#include "format_info.h"
+# include <stdint.h>
+# include "format_info.h"
 
 const t_string_flag_pair	*get_string_flag(const char *str);
-
 #endif
